33d7bc6ef1c9c2bd9fd7d618106dbb8a28c94bcc
sweeneyb <tab126@gmail.com>
2021-03-31 13:18:34 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "briansweeneydev",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
