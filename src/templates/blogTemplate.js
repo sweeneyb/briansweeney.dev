7559fb2b4450a474b1ce2285e716a1f26af2578b
sweeneyb <tab126@gmail.com>
2021-03-31 14:55:39 -0400

    container and global styles fighting

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,12 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-  backgroundColor: "grey"
-}
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -14,7 +8,7 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div style={pageStyles}>
+    <div>
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
